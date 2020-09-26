bb6e315ac5a1521467042b5beb1c134154a4cdf1
DavCam05 <davidecammarano2005@gmail.com>
2020-09-26 12:32:23 +0100

    Changed Activity for the bot

--- Services/CommandHandler.cs
+++ Services/CommandHandler.cs
@@ -58,7 +58,7 @@ namespace Brobot.Services
 
         public Task OnReady()
         {
-            _discord.SetGameAsync("Pre-Release");
+            _discord.SetGameAsync("Version 1.0.0");
             Console.WriteLine("Brobot is up and running!");
             Console.WriteLine($"Connected as: {_discord.CurrentUser.Username}#{_discord.CurrentUser.Discriminator}");
             return Task.CompletedTask;
