81a138b712f622096a75b6965ea2e3dbd721c7bc
DavCam05 <davidecammarano2005@gmail.com>
2020-11-07 17:29:40 +0000

    added kick and ban

--- Commands/ModCommands.cs
+++ Commands/ModCommands.cs
@@ -6,23 +6,50 @@ using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
+using IMDbApiLib.Models;
 using JikanDotNet;
 
 namespace Brobot.Commands
 {
     public class ModCommands : ModuleBase
     {
-        [Command("report")]
-        public async Task ReportUser(SocketGuildUser user = null) //still needs fixing
+        [RequireBotPermission(GuildPermission.BanMembers, ErrorMessage = "The bot does not have BanMembers permission")]
+        [RequireUserPermission(GuildPermission.BanMembers, ErrorMessage = "You need to have BanMembers permission to use this command")]
+        [Command("ban")]
+        public async Task BanUser(SocketGuildUser user)
         {
-            if(user == null)
-            {
-                await Context.Channel.SendMessageAsync("You need to specify a user to report");
-            }else
-            {
-                await Context.Channel.SendMessageAsync($":mega: {user.Mention} has been reported by {Context.User.Mention} for going against the rules of the server. Owner of the server: {Context.Guild.GetOwnerAsync().Result.Mention}");
-            }
-
+            await user.BanAsync();
+            await Context.Guild.AddBanAsync(user);
+            await Context.Channel.SendMessageAsync($"The user {user} has been banned by {Context.User.Username}. Next time follow those damn rules");
         }
+
+        [RequireBotPermission(GuildPermission.KickMembers, ErrorMessage = "The bot does not have KickMembers permission")]
+        [RequireUserPermission(GuildPermission.KickMembers, ErrorMessage = "You need to have the KickMembers permission")]
+        [Command("kick")]
+        public async Task KickUser(SocketGuildUser user)
+        {
+            await user.KickAsync();
+            await Context.Channel.SendMessageAsync($"The user {user} has been kicked by {Context.User.Username}. Next time follow those damn rules");
+        }
+
+        [RequireBotPermission(GuildPermission.ManageRoles, ErrorMessage = "The bot requires the ManageRoles permission")]
+        [RequireUserPermission(GuildPermission.ManageRoles, ErrorMessage = "You need to have the ManageRoles permission")]
+        [Command("addrole")]
+        public async Task AddRole(IRole role, SocketGuildUser user)
+        {
+            await user.AddRoleAsync(role);
+            await Context.Channel.SendMessageAsync($"The {role} role was added to {user}");
+        }
+
+        [RequireBotPermission(GuildPermission.ManageRoles, ErrorMessage = "The bot requires the ManageRoles permission")]
+        [RequireUserPermission(GuildPermission.ManageRoles, ErrorMessage = "You need to have the ManageRoles permission")]
+        [Command("remrole")]
+        public async Task RemoveRole(IRole role, SocketGuildUser user)
+        {
+            await user.RemoveRoleAsync(role);
+            await Context.Channel.SendMessageAsync($"The {role} role was removed from {user}");
+        }
+
+
     }
 }
